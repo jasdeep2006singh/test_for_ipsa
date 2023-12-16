987ffe74f71c458e29d297fb99179be5892fea1d
jasdeep2006singh <69591072+jasdeep2006singh@users.noreply.github.com>
2023-12-16 22:45:21 +0100

    Update seconds.c

--- seconds.c
+++ seconds.c
@@ -2,13 +2,15 @@
 #include <time.h>
 int main(){
     clock_t start_t, end_t, total_t;
+    int i=0;
     start_t = clock();
     while(1){
         end_t = clock();
         total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
          if (total_t==1){
-            printf("%d\n",total_t);
+            printf("%d\n",i);
             total_t=0;
+            i=i+1;
             start_t = clock();
         }
    }
