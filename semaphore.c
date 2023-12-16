6475b1c4c7a1850d9da99cfca371a2a7e85b3a9a
jasdeep2006singh <69591072+jasdeep2006singh@users.noreply.github.com>
2023-12-16 22:58:00 +0100

    Update semaphore.c

--- semaphore.c
+++ semaphore.c
@@ -1,8 +1,8 @@
 
-#include < stdio.h> 
-#include < pthread.h> 
-#include < semaphore.h> 
-#include < unistd.h> 
+#include <stdio.h> 
+#include <pthread.h> 
+#include <semaphore.h> 
+#include <unistd.h> 
 sem_t mutex; 
 void* thread(void* arg) 
 { 
