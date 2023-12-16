c58c054e5c43cfb8d3fe06985f63d8683c964bdd
jasdeep2006singh <69591072+jasdeep2006singh@users.noreply.github.com>
2023-12-16 21:57:22 +0100

    Create RR.c

---
+++ RR.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int task1(void);
+int task2(void);
+int task3(void);
+int main() {
+   while(True)
+   {
+        task1();
+        task2();
+        task3();
+   }
+}
+int task1(void)
+{
+    print("task1 \n");
+}
+int task2(void)
+{
+    print("task2 \n");
+}
+int task3(void)
+{
+    print("task3 \n");
+}
