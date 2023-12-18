8fbb18f2ea03dcfc1f941036172f15dcc94ad7bf
jasdeep2006singh <69591072+jasdeep2006singh@users.noreply.github.com>
2023-12-18 08:01:42 +0100

    Update linear_search.c

--- linear_search.c
+++ linear_search.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int list[]={2,34,12,55,69,1,10,22,45,36,99,13,56}
+    int list[]={2,34,12,55,69,1,10,22,45,36,99,13,56};
     for(int i=0; i<sizeof(list);i++)
     {
         if(list[i]==search_n)
