c27b191de360394ff0d90678ffbbf48e9bd77aff
jasdeep2006singh <69591072+jasdeep2006singh@users.noreply.github.com>
2023-12-18 08:00:11 +0100

    Create linear_search.c

---
+++ linear_search.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int list[]={2,34,12,55,69,1,10,22,45,36,99,13,56}
+    for(int i=0; i<sizeof(list);i++)
+    {
+        if(list[i]==search_n)
+        {
+            printf("Found %d at %d -th place",search_n,i);
+            return 0;
+        }
+    }
+}
